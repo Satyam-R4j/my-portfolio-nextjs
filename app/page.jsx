d6f6c322976b7a435e82315e045fdb87a3179ee2
Satyam-R4j <satyamraj418@gmail.com>
2025-09-12 20:56:30 +0530

    finished menus and theming, working on home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,18 @@
+import {Button} from "@/components/ui/button";
+import {FiDownload} from "react-icons/fi";
+
+
+
 
 export default function Home() {
   return (
-    <main >homepage this is it
-    </main>
+   <section className="h-full">
+    <div className="container mx-auto h-full">
+      <div className="flex flex-col xl:flex-col items-center justify-between xl:pt-8 xl:pb-24">
+        <div className="text-center ">asdf</div>
+      </div>
+    </div>
+
+   </section>
   );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,27 +1,34 @@
 import Link from "next/link";
-import {Button} from "./ui/button";
+import { Button } from "./ui/button";
 import Nav from "./Nav";
-
-
+import MobileNav from "./MobileNav";
 
 const Header = () => {
-  return <header className="py-8 xl:py-12 text-white bg-pink-50/20 ">
-    <div className="container  mx-auto flex justify-between ">
-      <Link href="/">
-        <h1 className="text-4xl font-semibold">Satyam-R4j<span className="text-red-500">.</span></h1>
-      </Link>
+  return (
+    <header className="py-8 xl:py-12 p-10 text-white bg-pink-50/20">
+      <div className="container mx-auto flex items-center justify-between">
+        {/* Logo (left) */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Satyam<span className="text-red-500">.</span>
+          </h1>
+        </Link>
 
-      <div className="hidden xl:flex items-center gap">
-      <Nav/>
-      <Link href="/contact">
-      <Button>Hire me</Button>
-      </Link>
-        
+        {/* Desktop Nav (right) */}
+        <div className="hidden xl:flex items-center gap-6">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+
+        {/* Mobile Nav Trigger (right on small screens) */}
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
       </div>
-
-      <div className="xl:hidden">mobile nav</div>
-    </div>
-  </header>;
+    </header>
+  );
 };
 
 export default Header;
